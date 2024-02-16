dfaa36771c49ff720b96aa999bf7822fcf6c7405
Kakashi Hatake <138500712+Kakashi069@users.noreply.github.com>
2024-02-16 23:37:18 +0530

    Update config.py

--- config.py
+++ config.py
@@ -11,46 +11,46 @@ from logging.handlers import RotatingFileHandler
 
 
 #Bot token @Botfather
-TG_BOT_TOKEN = os.environ.get("6467531922:AAEQoD3p0kQB9YTKZTHBkNU1m2NtHhgId9s", "")
+TG_BOT_TOKEN = os.environ.get("TG_BOT_TOKEN", "I")
 
 #Your API ID from my.telegram.org
-APP_ID = int(os.environ.get("27494774", ""))
+APP_ID = int(os.environ.get("APP_ID", "28774"))
 
 #Your API Hash from my.telegram.org
-API_HASH = os.environ.get("65f2c43c01f4757dcc875de35249829b", "")
+API_HASH = os.environ.get("API_HASH", "d8c8da4a6a96d044028e")
 
 #Your db channel Id
-CHANNEL_ID = int(os.environ.get("-1001649343645", ""))
+CHANNEL_ID = int(os.environ.get("CHANNEL_ID", "-10389388"))
 
 #OWNER ID
-OWNER_ID = int(os.environ.get("2136908929", ""))
+OWNER_ID = int(os.environ.get("OWNER_ID", "663201"))
 
 #Port
-PORT = os.environ.get("PORT", "8030")
+PORT = os.environ.get("PORT", "8080")
 
 #Database
-DB_URI = os.environ.get("mongodb+srv://SaturuGojo:SaturuGojo@cluster0.ikrb9xv.mongodb.net/?retryWrites=true&w=majority", "")
-DB_NAME = os.environ.get("SaturuGojo", "filesharexbot")
+DB_URI = os.environ.get("DATABASE_URL", "mongodb@cluster0.bk6llrv.mongodb.net/?retryWrites=true&w=majority")
 JOIN_REQS_DB = environ.get("JOIN_REQS_DB", DB_URI)
-
+DB_NAME = os.environ.get("DATABASE_NAME", "filesharexbot")
 
 #force sub channel id, if you want enable force sub
-FORCE_SUB_CHANNEL = int(os.environ.get("FORCE_SUB_CHANNEL", "-1002058531177"))
-FORCE_SUB_CHANNEL2 = int(os.environ.get("FORCE_SUB_CHANNEL2", "-1002058531177"))
+FORCE_SUB_CHANNEL = int(os.environ.get("FORCE_SUB_CHANNEL", "-10011373"))
+# Req channel id
+FORCE_SUB_CHANNEL2 = int(os.environ.get("FORCE_SUB_CHANNEL2", "-1829927"))
 
 TG_BOT_WORKERS = int(os.environ.get("TG_BOT_WORKERS", "4"))
 
 #start message
-START_MSG = os.environ.get("START_MESSAGE", "<b>Hello {first}\n\nI am a file store bot Powered by @Animes_xyz ⚡</b>.")
+START_MSG = os.environ.get("START_MESSAGE", "<b>Hello Pervs 🥵!!!!\n\nI Am A Special Bot For Your Lusty Thoughts. I Am Here To Give You Your Favourite Hanime In All Qualities In  Few Seconds 💦\n\nPowered By Hentai Campus ⚡.</b>.")
 try:
-    ADMINS=[6376328008]
-    for x in (os.environ.get("ADMINS", "6680981259").split()):
+    ADMINS=[6446763201]
+    for x in (os.environ.get("ADMINS", "6449").split()):
         ADMINS.append(int(x))
 except ValueError:
         raise Exception("Your Admins list does not contain valid integers.")
 
 #Force sub message 
-FORCE_MSG = os.environ.get("FORCE_SUB_MESSAGE", "<b>Sorry Dude ned MChann</b>\n\n<b>So Please Joinhaning Your Favourite Animes ⚡</b>")
+FORCE_MSG = os.environ.get("FORCE_SUB_MESSAGE", "<b>Sorry Dude You're Not Joined My Channel 😐</b>\n\n<b>So Please Join Our Update Channel To Continue Watching Your Favourite Hentai 🥵</b>")
 
 #set your Custom Caption here, Keep None for Disable Custom Caption
 CUSTOM_CAPTION = os.environ.get("CUSTOM_CAPTION", None)
@@ -65,7 +65,7 @@ BOT_STATS_TEXT = "<b>BOT UPTIME</b>\n{uptime}"
 USER_REPLY_TEXT = "🚫 Please Avoid Direct Messages. I'm Here merely for file sharing!"
 
 ADMINS.append(OWNER_ID)
-ADMINS.append(6208886200)
+ADMINS.append(6446763201)
 
 LOG_FILE_NAME = "filesharingbot.txt"
 
